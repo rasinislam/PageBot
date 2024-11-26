fc7ea1a10c491106c0eea873f6b7babbf5df67de
Gestures5 <kenshimadax@gmail.com>
2024-11-27 00:46:38 +0800

    Update bb.js

--- commands/bb.js
+++ commands/bb.js
@@ -28,7 +28,7 @@ const handleChatResponse = async (senderId, input, pageAccessToken) => {
   const apiUrl = "https://api.kenliejugarap.com/blackbox-gpt4o/?";
 
   try {
-    const { data } = await axios.get(apiUrl, { params: { text: input } });
+    const { data } = await axios.get(apiUrl, { params: { text: question} });
     let response = data.response;
 
     sendMessage(senderId, { text: 'Answering your question...' }, pageAccessToken);
