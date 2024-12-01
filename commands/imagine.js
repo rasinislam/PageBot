dcd37d90d81ae56ee4bfd998c6be35814c2a56fe
Gestures5 <kenshimadax@gmail.com>
2024-12-01 23:24:06 +0800

    Update imagine.js

--- commands/imagine.js
+++ commands/imagine.js
@@ -14,7 +14,7 @@ module.exports = {
     const prompt = args.join(' ');
 
     try {
-      const apiUrl = `https://appjonellccapis.zapto.org/api/generate-art?prompt=${encodeURIComponent(prompt)}`;
+      const apiUrl = `https://ccprojectapis.ddns.net/api/generate-art?prompt=${encodeURIComponent(prompt)}`;
 
       await sendMessage(senderId, { attachment: { type: 'image', payload: { url: apiUrl } } }, pageAccessToken);
 
